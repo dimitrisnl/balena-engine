8a91889e6e4065f5cc22d0e8df8bf44becf3a3e2
Robert Günzler <r@gnzler.io>
2019-05-10 20:08:04 +0200

    test: test if we can still run a previously created contaiener

--- cmd/a2o-migrate/test/integration.sh
+++ cmd/a2o-migrate/test/integration.sh
@@ -13,38 +13,42 @@ set -x
     # start balenaEngine with aufs
     $RT run $balena_container_flags $IMAGE --debug --storage-driver=aufs
     sleep 1
-    $RT inspect balena || exit 1
+    $RT inspect balena &>/dev/null || exit 1
+
+    cat test/Dockerfile | $RT exec -i balena balena-engine build -t a2o-test -
+    $RT exec balena balena-engine run --name a2o-test-container a2o-test ls -lR /tmp
 
     # run migration
     $RT exec -it balena /src/test/$(basename $0)
+
+    # check if we can still run from the aufs image
+    $RT exec balena balena-engine run --rm a2o-test ls -lR /tmp
+    # stop aufs daemon
     $RT stop -t 3 balena
 
     # start balenaEngine with overlay2
     $RT run $balena_container_flags $IMAGE --debug --storage-driver=overlay2
     sleep 1
-    $RT inspect balena || exit 1
+    $RT inspect balena &>/dev/null || exit 1
 
     # check if we still are able to create a container from the a2o-test image
-    $RT exec -it balena balena-engine run --rm a2o-test ls -lR /tmp
+    $RT exec balena balena-engine run --rm a2o-test ls -lR /tmp
+    # check if rewriting the container storage drivers worked
+    $RT exec balena balena-engine start a2o-test-container
+
+    # cleanup
     $RT stop -t 3 balena
     $RT volume rm -f varlibbalena
     exit 0
 }
 
 cat /etc/os-release
-
 balena-engine info || exit 1
 
-cat test/Dockerfile | balena-engine build -t a2o-test -
-
-balena-engine run --rm a2o-test ls -lR /tmp
-
-ls -l /var/lib/balena-engine/aufs/
-ls -l /var/lib/balena-engine/overlay2/
+# ls -lR /var/lib/balena-engine/
 
 ./a2o-migrate -version
 
 ./a2o-migrate -debug
 
-ls -l /var/lib/balena-engine/aufs/
-ls -l /var/lib/balena-engine/overlay2/
+# ls -lR /var/lib/balena-engine/
